718debc980dd55c16fb8ff724ab062886bc798cc
Eddie Chu <contact@eddiechu.me>
2019-09-05 21:05:16 -0700

    add info-window functionality

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
 
 import { API_GOOGLE_MAPS } from "../config";
 
@@ -11,28 +11,69 @@ const mapStyles = {
 };
 
 export class GoogleMap extends React.Component {
-    
+    state = {
+        activeMarker: {},
+        selectedPlace: {},
+        showingInfoWindow: false
+    };
+
+    onMarkerClick = (props, marker) =>
+        this.setState({
+            activeMarker: marker,
+            selectedPlace: props,
+            showingInfoWindow: true
+        });
+
+    onInfoWindowClose = () =>
+        this.setState({
+            activeMarker: null,
+            showingInfoWindow: false
+        });
+
+    onMapClicked = () => {
+        if (this.state.showingInfoWindow)
+            this.setState({
+                activeMarker: null,
+                showingInfoWindow: false
+            });
+    };
+
     render() {
+        console.log(this.state.selectedPlace);
         let markers = this.props.vendors
             ? this.props.vendors.map((vendor, index) => {
                   return (
                       <Marker
+                          {...vendor}
+                          name={`Marker ${index}`}
                           key={index}
                           id={index}
                           position={{
                               lat: vendor.Latitude,
                               lng: vendor.Longitude
                           }}
-                          onClick={() => console.log("You clicked me!")}
-                      >
-                      </Marker>
+                          onClick={this.onMarkerClick}
+                      ></Marker>
                   );
               })
             : null;
 
         return (
-            <Map google={this.props.google} zoom={13} style={mapStyles} initialCenter={this.props.mapCenter} center={this.props.mapCenter} >
+            <Map google={this.props.google} zoom={13} style={mapStyles} initialCenter={this.props.mapCenter} center={this.props.mapCenter}>
                 {markers ? markers : null}
+
+                <InfoWindow marker={this.state.activeMarker} onClose={this.onInfoWindowClose} visible={this.state.showingInfoWindow}>
+                    <div className="info-window">
+                        <h2 className="heading-secondary">{this.state.selectedPlace.Vendor}</h2>
+                        <address className="info-window__address">
+                            {this.state.selectedPlace.Address}
+                            <br />
+                            {this.state.selectedPlace.City}
+                            <br />
+                            {this.state.selectedPlace["Zip Code"]}
+                        </address>
+                    </div>
+                </InfoWindow>
             </Map>
         );
     }
